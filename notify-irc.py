f556077f752a02b8168167df58309d88ba5543ec
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-01-11 23:33:53 +0200

    Modify to send commit messages to irc instead of email

--- notify-irc.py
+++ notify-irc.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python
 
 import sys
+import os
 import urllib, urllib2
 import re
 import subprocess
 from datetime import datetime
-import simplejson as json
+import socket
 
 
-POST_URL = 'http://example.com'
-REPO_URL = 'http://example.com'
+REPO_HOST = "codegrove.org"
 COMMIT_URL = r'http://example.com/commit/%s'
-REPO_NAME = 'gitrepo'
-REPO_OWNER_NAME = 'Git U. Some'
-REPO_OWNER_EMAIL = 'git@example.com'
-REPO_DESC = ''
-
 
 EMAIL_RE = re.compile("^(.*) <(.*)>$")
+
+
+IRCCAT_HOST = "localhost"
+IRCCAT_TCP_PORT = 3011
+
+
+
+class Irccat(object):
+    """
+    dead simple bridge to irccat
+    http://irccat.rubyforge.org/use.html
+    """
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def say(self, msg):
+        """say something to irc"""
+        print "saying to irc %s"  % msg
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.port))
+        s.send(msg)
+        s.close()
 
 
 def get_revisions(old, new):
@@ -27,77 +46,70 @@ def get_revisions(old, new):
     revisions = []
     s = 0
     while s < len(sections):
-	lines = sections[s].split('\n')
-	    
-	# first line is 'commit HASH\n'
-	props = {'id': lines[0].strip().split(' ')[1]}
-	    
-	# read the header
-	for l in lines[1:]:
-	    key, val = l.split(' ', 1)
-	    props[key[:-1].lower()] = val.strip()
+        lines = sections[s].split('\n')
 
-	# read the commit message
-	props['message'] = sections[s+1]
+        # first line is 'commit HASH\n'
+        props = {'id': lines[0].strip().split(' ')[1]}
 
-	# use github time format
-	basetime = datetime.strptime(props['date'][:-6], "%a %b %d %H:%M:%S %Y")
-	tzstr = props['date'][-5:]
-	props['date'] = basetime.strftime('%Y-%m-%dT%H:%M:%S') + tzstr
+        # read the header
+        for l in lines[1:]:
+            key, val = l.split(' ', 1)
+            props[key[:-1].lower()] = val.strip()
 
-	# split up author
-	m = EMAIL_RE.match(props['author'])
-	if m:
-	    props['name'] = m.group(1)
-	    props['email'] = m.group(2)
-	else:
-	    props['name'] = 'unknown'
-	    props['email'] = 'unknown'
-	del props['author']
-	
-	revisions.append(props)
-	s += 2
-	
+        # read the commit message
+        props['message'] = sections[s+1]
+
+        # use github time format
+        basetime = datetime.strptime(props['date'][:-6], "%a %b %d %H:%M:%S %Y")
+        tzstr = props['date'][-5:]
+        props['date'] = basetime.strftime('%Y-%m-%dT%H:%M:%S') + tzstr
+
+        # split up author
+        m = EMAIL_RE.match(props['author'])
+        if m:
+            props['name'] = m.group(1)
+            props['email'] = m.group(2)
+        else:
+            props['name'] = 'unknown'
+            props['email'] = 'unknown'
+        del props['author']
+
+        revisions.append(props)
+        s += 2
+
     return revisions
 
-def make_json(old, new, ref):
-    data = {
-	'before': old,
-	'after': new,
-	'ref': ref,
-	'repository': {
-	    'url': REPO_URL,
-	    'name': REPO_NAME,
-	    'description': REPO_DESC,
-	    'owner': {
-		'name': REPO_OWNER_NAME,
-		'email': REPO_OWNER_EMAIL
-		}
-	    }
-	}
+def get_commits(old, new, ref):
 
     revisions = get_revisions(old, new)
     commits = []
     for r in revisions:
-	commits.append({'id': r['id'],
-			'author': {'name': r['name'], 'email': r['email']},
-			'url': COMMIT_URL % r['id'],
-			'message': r['message'],
-			'timestamp': r['date']
-			})
-    data['commits'] = commits
-
-    return json.dumps(data)
-
-
-def post(url, data):
-    u = urllib2.urlopen(POST_URL, urllib.urlencode({'payload': data}))
-    u.read()
-    u.close()
+        commits.append({
+                'id': r['id'],
+                'author': {'name': r['name'], 'email': r['email']},
+                'url': COMMIT_URL % r['id'],
+                'message': r['message'],
+                'timestamp': r['date']
+            })
+    return commits
 
 
-if __name__ == '__main__':
+def make_irc_friendly(commit_msg):
+    first_line = (commit_msg or "").split("\n")[0]
+    return first_line.strip()
+
+def main():
+    irc = Irccat(IRCCAT_HOST, IRCCAT_TCP_PORT)
     for line in sys.stdin.xreadlines():
         old, new, ref = line.strip().split(' ')
-        data = make_json(old, new, ref)
-        post(POST_URL, data)
+
+        for commit in  get_commits(old, new, ref):
+            irc.say("%s commited '%s' to ssh://%s:%s" % (
+                commit['author']['name'],
+                make_irc_friendly(commit['message']),
+                REPO_HOST,
+                os.getcwd(),
+            ))
+
+if __name__ == '__main__':
+    main()
